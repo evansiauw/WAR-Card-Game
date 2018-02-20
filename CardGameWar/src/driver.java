67800526ca06a6e9beec3222f192bc4ba71cadde
Iwan Siauw <evansiauw@gmail.com>
2018-02-20 13:22:38 -0500

    a

--- CardGameWar/src/driver.java
+++ CardGameWar/src/driver.java
@@ -5,14 +5,11 @@ public class driver {
 	public static void main(String[] args) {
 	
 		Deck array [] = new Deck[52];
-		
 		String [] ranks = {"2","3","4","5","6","7","8","9","10","Jack","Queens","King","Ace"};
 		
 		for(int i=0; i<52; i++) {
-			
-			array [i] = new Deck(ranks[i%13],(i+2)%12);
-			
-			}
+			array [i] = new Deck(ranks[i%13],(i+2)%12); 
+		}	
 		
         System.out.println("Before Swapping");
 		for (int i = 0; i < array.length; i++) {
