2c9eaba9704692b80747bf2ca5fcb9685adebf97
Iwan Siauw <evansiauw@gmail.com>
2018-02-20 12:05:28 -0500

    a

--- CardGameWar/src/Deck.java
+++ CardGameWar/src/Deck.java
@@ -2,5 +2,23 @@ import java.util.LinkedList;
 
 public class Deck {
 	
+	private String cardName;
+	private int cardValue;
+	
+	public Cards(String cardRank, int cardValue) {
+		
+		cardName = cardRank;
+		this.cardValue = cardValue;
+	}
+	
+	public String getName () { return cardName; }
+	public int getValue () { return cardValue; }
+	
+	public void setName (String Name) { cardName = Name; }
+	public void setValue (int Value) { cardValue = Value; }
+	
+	public String toString() {
+		return cardName;
+	}
 	
 }
--- CardGameWar/src/driver.java
+++ CardGameWar/src/driver.java
@@ -1,9 +1,19 @@
 import java.util.LinkedList;
+
 public class driver {
 
 	public static void main(String[] args) {
+	
+		Deck array [] = new Deck[52];
 		
-
+		String [] ranks = {"2","3","4","5","6","7","8","9","10","Jack","Queens","King","Ace"};
+		
+		for(int i=0; i<13; i++) {
+			
+			array [i] = new Deck(ranks[i],i+2);
+			
+			}
+		
 	    LinkedList<Integer> list = new LinkedList<>();
 
 	
