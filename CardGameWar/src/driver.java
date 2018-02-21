607baf39560dcbe2d17065fa11f0fe9879eb193a
Iwan Siauw <evansiauw@gmail.com>
2018-02-21 12:57:18 -0500

    final changes

--- CardGameWar/src/driver.java
+++ CardGameWar/src/driver.java
@@ -95,6 +95,10 @@ public class driver {
                 temp.clear();
             }
     		
+            else {
+            		NoWar(player1,player2,temp);
+            }
+    		
     		
     }
     
